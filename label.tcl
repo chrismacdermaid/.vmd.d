7a8186823b96a9a7ed49b4002a1cddefd70e7f94
Chris MacDermaid <chris.macdermaid@gmail.com>
2013-08-13 10:55:47 -0400

    added atom labeling routineto vmd autoload scripts

---
+++ label.tcl
@@ -0,0 +1,33 @@
+
+proc labelatoms {sel label {size 1.0} {off {0.5 0.5 0.0}}} {
+
+    set molid [$sel molid]
+
+    if {[lsearch -ascii [atomselect keywords] $label] == -1} {
+	set msg [vmdcon -err "Unknown atomselect keyword"]
+	return -code error $msg
+    }
+
+    ## Get the viewpoint for the mol we're labeling
+    save_viewpoint 99999 $molid
+
+    ## Make a new mol
+    set newmol [mol new]
+
+    ## Set the label color to white
+    graphics $newmol color 8
+
+    foreach x [$sel get x]\
+	y [$sel get y]\
+	z [$sel get z]\
+	w [$sel get $label] {
+	    graphics $newmol text [vecadd [list $x $y $z] $off] $w size $size
+	}
+
+    ## Make sure the old mol is top
+    molinfo $molid set top 1
+
+    ## Apply the viewpoint to the new mol
+    cpy_viewpoint 99999 $molid $newmol
+
+}
