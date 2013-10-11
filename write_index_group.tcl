1acd13d8f3b2c7d4b3ae478e8c161fd8a97f5243
Chris MacDermaid <chris.macdermaid@gmail.com>
2013-10-11 09:03:30 -0400

    added eos ssh host, add misc.tcl commands for vmd autoload, some emacs tweaks

--- write_index_group.tcl
+++ write_index_group.tcl
@@ -1,20 +1,30 @@
-proc write_index_group { output sel label { n_atoms_max 1000000000 } } {
+proc write_index_group { outfile sel label { n_atoms_max 1000000000 } } {
     if { [${sel} num] == 0 } return
-    puts -nonewline ${output} [format "\[ %s \]\n" ${label}]
+
+    ## Open file
+    set fid [open $outfile w]
+
+    ## Write out
+    puts -nonewline ${fid} [format "\[ %s \]\n" ${label}]
     set line_buf 0
     set atom_count 0
     foreach num [${sel} get serial] {
         incr atom_count
         if { ${atom_count} > ${n_atoms_max} } break
-        puts -nonewline ${output} [format " %9d" ${num}]
+        puts -nonewline ${fid} [format " %9d" ${num}]
         set line_buf [expr ${line_buf} + 10]
         if { ${line_buf} > 70 } {
             set line_buf 0
-            puts -nonewline ${output} "\n"
+            puts -nonewline ${fid} "\n"
         }
     }
+
     if { ${line_buf} > 0 } {
-        puts -nonewline ${output} "\n"
+        puts -nonewline ${fid} "\n"
     }
-    puts -nonewline ${output} "\n"
+
+    puts -nonewline ${fid} "\n"
+
+    ## Close file
+    close $fid
 }
