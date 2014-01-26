a4b31413278e3eb02f289bbb474127ee8297c421
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-01-26 10:45:25 -0500

    removed cpk.tcl routine from misc.tcl

--- misc.tcl
+++ misc.tcl
@@ -11,29 +11,3 @@ proc nres {sel} {return [llength [lsort -unique [$sel get residue]]]}
 
 ## Run scripts silently.
 proc source_silent {args} {foreach f $args {source $f}}
-
-## Use a repid id and create 3 representations:
-## 1. seltext and "C.*"
-## 2. seltext and "O.*"
-## 3. seltext and "N.*"
-
-proc cpk {repid {molid top}} {
-
-    if {$molid == "top"} {
-        set molid [molinfo top]
-    }
-
-    ## Molinfo properties
-    set rep{}
-    foreach x {rep color selection color material} {
-        lappend rep [molinfo get $molid "{$x $repid}"]
-    } 
-
-    ## Molproperties
-    foreach x {numperiodic showperiodic selupdate
-        colupdate scaleminmax smoothrep drawframes} {
-        lappend rep [mol $x $molid $repid]
-    }
-
-}
-
