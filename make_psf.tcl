cc695f92560e0fc8e66578fd91f4de2741844662
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-02-20 21:22:41 -0500

    updated psf script to work on multiple molecules at once

--- make_psf.tcl
+++ make_psf.tcl
@@ -1,14 +1,16 @@
 ## Make a psf file from a molid.Try to be intellegent
 ## about the fragments, segments etc.. No guarantees!
 ## check it when it's done.
-proc make_psf {mol {topologies def}} {
+proc make_psf {mollist {topologies def}} {
 
     global env
 
     package require psfgen
 
     ## Reanalyze mol since we use some built in vmd macros below
-    mol reanalyze $mol
+    foreach mol $mollist {
+        mol reanalyze $mol
+    }
 
     ## Cleanup psfgen
     psfcontext reset
@@ -25,154 +27,162 @@ proc make_psf {mol {topologies def}} {
         topology $top
     }
 
-    ## Identify molecules with the same resnames, do waters
-    ## separately since we need a lot of them per segment
-
-    set sel [atomselect $mol "not (water or protein)"]
-    set resnames [lsort -unique -ascii [$sel get resname]]
-
-    set fragsperseg_list {}
-    set opt_list {}
-    if {[$sel num] > 0} {
-        set fragsperseg_list [lrepeat [llength $resnames] 2000]
-        set opt_list [lrepeat [llength $resnames] ""]
-    }
-
-    $sel delete
-
-    set sel [atomselect $mol "water"]
-    set solventnames [lsort -unique -ascii [$sel get resname]]
-    $sel delete
-
-    ## Special options for water
-    if {[llength $solventnames] > 0} {
-        set resnames [list {*}$resnames $solventnames]
-        lappend fragsperseg_list 5000
-        lappend opt_list "auto none"
-    }
-
+    ## Process each molid into a single psf
     ## Unique segment identifier per-residue name
     set segidx 0
     set segidx2 0
 
-    foreach rn $resnames fragperseg $fragsperseg_list opt $opt_list {
+    foreach mol $mollist {
 
-        set sel [atomselect $mol "resname $rn"]
-        set fragments [lsort -unique -increasing -integer -index 0 [$sel get fragment]]
+        ## Identify molecules with the same resnames, do waters
+        ## separately since we need a lot of them per segment
+
+        set sel [atomselect $mol "not (water or protein)"]
+        set resnames [lsort -unique -ascii [$sel get resname]]
+
+        set fragsperseg_list {}
+        set opt_list {}
+        if {[$sel num] > 0} {
+            set fragsperseg_list [lrepeat [llength $resnames] 2000]
+            set opt_list [lrepeat [llength $resnames] ""]
+        }
+
         $sel delete
 
-        ## Unique segment index fragment size
-        set segidx2 0
+        set sel [atomselect $mol "water"]
+        set solventnames [lsort -unique -ascii [$sel get resname]]
+        $sel delete
 
-        ## Total number of fragments with this residue name
-        set nfragments [llength $fragments]
-
-        for {set off 0} {$off < $nfragments} {incr off $fragperseg} {
-
-            #Set the segment name to residue_seg_idx + seg_index
-            set segname "$segidx$segidx2"
-
-            ## Make the segments each having a maximum of fragsperseg fragments
-            set subfragments [lrange $fragments $off [expr {$off+$fragperseg-1}]]
-
-            segment $segname {
-
-                set outname [format "%s_%s.pdb" $segname $rn]
-                set sel [atomselect $mol "fragment $subfragments"]
-
-                ## Renumber the resids so they don't overlap.
-                set n1 [$sel num]; set n2 [llength $subfragments]
-
-                ## Check if n2 cleanly divides n1 (no missing atoms),
-                ## this is not fool-proof but should work most of the
-                ## time
-                if {[expr {$n1 % $n2}] != 0} { #; Slow, but OK for missing atoms
-                    set resid 1
-                    foreach f $subfragments {
-                        set sel2 [atomselect $mol "fragment $f"]
-                        $sel2 set resid $resid
-                        $sel2 delete
-                        incr resid
-                    }
-
-                } else { #; Fast, but assumes all fragments have the same number of atoms
-
-                    set n2 [expr {$n1 / $n2}]; # This assumes no missing atoms...
-                    set resids {}
-                    for {set i 1; set j 0} {$j < $n1} {incr i; incr j $n2} {lappend resids [lrepeat $n2 $i]};
-                    $sel set resid [join $resids]
-                }
-
-                ## Writeout the PDB for the collection of fragments
-                $sel set segname $segname
-                $sel writepdb $outname
-                $sel delete
-
-                ## Segment dependent opts
-                eval $opt
-
-                ## Read pdb
-                pdb $outname
-
-                first none
-                last none
-            }
-
-            ## Load the coordinates into the segments
-            set inname [format "%s_%s.pdb" $segname $rn]
-            coordpdb $inname $segname
-
-            ## delete the file since we don't need it anymore
-            file delete -force $inname
-
-            incr segidx2
+        ## Special options for water
+        if {[llength $solventnames] > 0} {
+            set resnames [list {*}$resnames $solventnames]
+            lappend fragsperseg_list 5000
+            lappend opt_list "auto none"
         }
 
-        incr segidx
-    }
+        foreach rn $resnames fragperseg $fragsperseg_list opt $opt_list {
 
-    set sel [atomselect $mol "protein"]
-    set protein_fragments [lsort -unique -integer -increasing [$sel get fragment]]
-    $sel delete
-
-    ## Each protein gets its own segment
-    if {[llength $protein_fragments] > 0} {
-        foreach fragment $protein_fragments {
-
-            #Set the segment name to residue_seg_idx + seg_index
-            set segname "$segidx$segidx2"
-
-            set outname [format "%s_%s.pdb" $segname $fragment]
-            set sel [atomselect $mol "fragment $subfragments"]
-
-            ## Writeout the PDB for the collection of fragments
-            $sel set segname $segname
-            $sel writepdb $outname
+            set sel [atomselect $mol "resname $rn"]
+            set fragments [lsort -unique -increasing -integer -index 0 [$sel get fragment]]
             $sel delete
 
-            ## Read pdb
-            pdb $outname
+            ## Unique segment index fragment size
+            set segidx2 0
 
-            first NTER
-            last CTER
+            ## Total number of fragments with this residue name
+            set nfragments [llength $fragments]
 
-            coordpdb $outname $segname
+            for {set off 0} {$off < $nfragments} {incr off $fragperseg} {
 
-            ## delete the file since we don't need it anymore
-            file delete -force $outname
+                #Set the segment name to residue_seg_idx + seg_index
+                set segname "$segidx$segidx2"
 
-            incr segidx2
+                ## Make the segments each having a maximum of fragsperseg fragments
+                set subfragments [lrange $fragments $off [expr {$off+$fragperseg-1}]]
+
+                segment $segname {
+
+                    set outname [format "%s_%s.pdb" $segname $rn]
+                    set sel [atomselect $mol "fragment $subfragments"]
+
+                    ## Renumber the resids so they don't overlap.
+                    set n1 [$sel num]; set n2 [llength $subfragments]
+
+                    ## Check if n2 cleanly divides n1 (no missing atoms),
+                    ## this is not fool-proof but should work most of the
+                    ## time
+                    if {[expr {$n1 % $n2}] != 0} { #; Slow, but OK for missing atoms
+                        set resid 1
+                        foreach f $subfragments {
+                            set sel2 [atomselect $mol "fragment $f"]
+                            $sel2 set resid $resid
+                            $sel2 delete
+                            incr resid
+                        }
+
+                    } else { #; Fast, but assumes all fragments have the same number of atoms
+
+                        set n2 [expr {$n1 / $n2}]; # This assumes no missing atoms...
+                        set resids {}
+                        for {set i 1; set j 0} {$j < $n1} {incr i; incr j $n2} {lappend resids [lrepeat $n2 $i]};
+                        $sel set resid [join $resids]
+                    }
+
+                    ## Writeout the PDB for the collection of fragments
+                    $sel set segname $segname
+                    $sel writepdb $outname
+                    $sel delete
+
+                    ## Segment dependent opts
+                    eval $opt
+
+                    ## Read pdb
+                    pdb $outname
+
+                    first none
+                    last none
+                }
+
+                ## Load the coordinates into the segments
+                set inname [format "%s_%s.pdb" $segname $rn]
+                coordpdb $inname $segname
+
+                ## delete the file since we don't need it anymore
+                file delete -force $inname
+
+                incr segidx2
+            }
+
+            incr segidx
         }
+
+        set sel [atomselect $mol "protein"]
+        set protein_fragments [lsort -unique -integer -increasing [$sel get fragment]]
+        $sel delete
+
+        ## Each protein gets its own segment
+        if {[llength $protein_fragments] > 0} {
+            foreach fragment $protein_fragments {
+
+                #Set the segment name to residue_seg_idx + seg_index
+                set segname "$segidx$segidx2"
+
+                segment $segname {
+                    set outname [format "%s_%s.pdb" $segname $fragment]
+                    set sel [atomselect $mol "fragment $fragment"]
+
+                    ## Writeout the PDB for the collection of fragments
+                    $sel set segname $segname
+                    $sel writepdb $outname
+                    $sel delete
+
+                    ## Read pdb
+                    pdb $outname
+
+                    first NTER
+                    last CTER
+                }
+                coordpdb $outname $segname
+
+                ## delete the file since we don't need it anymore
+                file delete -force $outname
+
+                incr segidx2
+            }
+        }
+        incr segidx
     }
 
     ## Guess missing coordinates
     guesscoord
 
-    lassign [molinfo $mol get name] pdb
-    set fname [file rootname $pdb]
+    foreach mol $mollist {
+        lassign [molinfo $mol get name] pdb
+        lappend names [file rootname $pdb]
+    }
 
+    set fname [join $names "-"]
     set seconds [clock seconds]
-
     set psf $fname\_$seconds\.psf
     set pdb $fname\_$seconds\.pdb
 
