68f226d15ec1c0bfd01817d527b0166d2e7b2ce3
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-05-06 09:16:20 -0400

    Added NAVb macro

--- atomselect_macros.tcl
+++ atomselect_macros.tcl
@@ -23,3 +23,5 @@ atomselect macro lipid { resname DLPE DMPC GPC LPPC PALM PC PGCL POPC POUC POPE 
 atomselect macro lnoh { lipid noh }
 
 atomselect macro pchead {lipid and name N C13 C14 C15 C12 C11 P O13 O14 O11 O12 C1 C2 O21 C21 O22 C22 C3 O31 C31 C23 }
+
+## NAVAB
