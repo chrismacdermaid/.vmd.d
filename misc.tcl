7de9b6358ca2c4cc8eede29ca7a1c9ab948338b6
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-07-20 08:08:18 -0400

    Added recenter function for trajectory processing

--- misc.tcl
+++ misc.tcl
@@ -12,9 +12,10 @@ proc nres {sel} {return [llength [lsort -unique [$sel get residue]]]}
 ## Run scripts silently.
 proc source_silent {args} {foreach f $args {source $f}}
 
-proc setbox {{molid top}} {
+proc setbox {{molid top} {tweak {0.0 0.0 0.0}}} {
     set sel [atomselect $molid "all"]
     set box [vecinvert [vecsub {*}[measure minmax $sel -withradii]]]
+    set box [vecadd $box $tweak]
     molinfo $molid set {a b c} $box
     $sel delete
 }
@@ -26,9 +27,8 @@ proc writebox {fname {molid top} {guess 0}} {
     }
 
     if {$guess} {setbox $molid}
-    pbc writexst $fname
+    pbc writexst $fname -molid $molid -now
 }
-
 
 ## Alias atomselect. I'm so tired of typing "atomselect"
 proc as_alias {} {
@@ -36,3 +36,29 @@ proc as_alias {} {
     interp alias {} ast {} atomselect top
     interp alias {} asa {} atomselect top all
 }; as_alias
+
+## calculate COM of sel1 translate $sel2 by inverse of COM 
+proc recenter {sel1 sel2 {first -1} {last -1}} {
+
+    set molid [$sel1 molid]  
+    if {$molid != [$sel2 molid]} {
+        vmdcon -err "Selections must share the same molid" 
+        return -1
+    }
+
+    if {$first < 0} {
+      set first [molinfo $molid get frame] 
+    }
+    
+    set n [molinfo $molid get numframes]    
+    if {$last < 0 || $last > $n} {
+      set last [molinfo $molid get frame] 
+    }
+
+    for {set i $first} {$i <= $last} {incr i} { 
+      molinfo $molid set frame $i
+      $sel1 update; $sel2 update
+      set v [vecinvert [measure center $sel1 weight mass]]
+      $sel2 moveby $v
+  }
+}
