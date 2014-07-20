d640806bd157471852ea1d400c3ed153cb30b819
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-07-20 08:08:43 -0400

    Added ability to render multiple mols at once

--- render.tcl
+++ render.tcl
@@ -2,14 +2,20 @@
 
 proc myrender {molid filename {start 0} {stop end} {stride 1}} {
 
+    if {$molid == "all"} {set molid [molinfo list]}
+
     if {$stop == "end"} {
-      set stop [molinfo $molid get numframes]
+        foreach m $molid {
+            lappend N [molinfo $m get numframes]
+        }
+        set N [lsort -integer -decreasing $N]
+        set stop [lindex $N 0]
     }
 
     for {set i $start;set j 0} {$i < $stop} {incr i $stride; incr j} {
-        molinfo $molid set frame $i
+        foreach m $molid {
+            molinfo $m set frame $i
+        }
         render TachyonInternal [format "%s.%04d.tga" $filename $j]
     }
-
 }
-
