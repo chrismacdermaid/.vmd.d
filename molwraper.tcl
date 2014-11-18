bcdef4774a61c7e40ca4d3b80dae63811253ad0e
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-11-17 21:32:59 -0500

    Added an xsc/xst option to the moll wrapper.

--- molwraper.tcl
+++ molwraper.tcl
@@ -37,15 +37,35 @@ proc moll { args } {
             }
         }
 
-        reload {
-            if {[lsearch -ascii [info procs] reload] < 0} {
-                return -code 1 "Reload command currently unavailable"
-            }
+	xst -
+        xsc {
 
-            set retval [reload {*}$newargs]
-        }
+            ## Load an XSC file, quite a bit more convenient than pbctools
+	    set filename [lindex $newargs 0]
+	    if {[catch {exec tail -1 $filename} data]} {
+		vmdcon -err "$data"
+		return -code error -1
+	    }
 
-        default {set retval [mol {*}$args]}
+	    lassign $data id ax ay az bx by bz cx cy cz
+	    if {[catch {molinfo top set {a b c} [list $ax $by $cz]} msg]} {
+		vmdcon -err "Can't set box dimensions: $msg"
+		return -code error -1
+	    }
+
+	    ## Provide the dimensions as a return value
+	    set retval [list $ax $by $cz]
+	}
+
+	reload {
+	    if {[lsearch -ascii [info procs] reload] < 0} {
+		return -code 1 "Reload command currently unavailable"
+	    }
+
+	    set retval [reload {*}$newargs]
+	}
+
+	default {set retval [mol {*}$args]}
 
     }
 
