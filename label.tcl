182a420cb3f5108d725a6d0e88bab8895e6b1d82
Chris MacDermaid <chris.macdermaid@gmail.com>
2015-01-14 11:48:46 -0500

    Fixed whitespace errors

--- label.tcl
+++ label.tcl
@@ -3,25 +3,25 @@
 ## Colorid: 8=white 16=black
 # labelatoms "all" {name type}
 proc labelatoms {{sel ""} {label name} {color 8}\
-		     {size 1.0} {off {0.5 0.5 0.0}}} {
+                     {size 1.0} {off {0.5 0.5 0.0}}} {
 
     set flag 0
 
     if {$sel == ""} {
-	set sel [atomselect top "all"]
-	set flag 1
+        set sel [atomselect top "all"]
+        set flag 1
     } elseif {![string match "atomselect*" $sel]} {
-	if {[catch {atomselect top $sel} sel]} {return}
-	set flag 1
+        if {[catch {atomselect top $sel} sel]} {return}
+        set flag 1
     } else {}
-    
+
     set molid [$sel molid]
 
     foreach x $label {
-	if {[lsearch -ascii [atomselect keywords] $x] == -1} {
-	    set msg [vmdcon -err "Unknown atomselect keyword: $x"]
-	    return -code error $msg
-	}
+        if {[lsearch -ascii [atomselect keywords] $x] == -1} {
+            set msg [vmdcon -err "Unknown atomselect keyword: $x"]
+            return -code error $msg
+        }
     }
 
     ## Get the viewpoint for the mol we're labeling
@@ -34,11 +34,11 @@ proc labelatoms {{sel ""} {label name} {color 8}\
     graphics $newmol color $color
 
     foreach x [$sel get x]\
-	y [$sel get y]\
-	z [$sel get z]\
-	w [$sel get $label] {
-	    graphics $newmol text [vecadd [list $x $y $z] $off] [join $w " "] size $size
-	}
+        y [$sel get y]\
+        z [$sel get z]\
+        w [$sel get $label] {
+            graphics $newmol text [vecadd [list $x $y $z] $off] [join $w " "] size $size
+        }
 
     ## Make sure the old mol is top
     molinfo $molid set top 1
@@ -47,6 +47,6 @@ proc labelatoms {{sel ""} {label name} {color 8}\
     cpy_viewpoint 99999 $molid $newmol
 
     if {[string match "atomselect*" $sel] && $flag} {
-	$sel delete
+        $sel delete
     }
 }
