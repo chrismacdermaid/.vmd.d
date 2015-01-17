5af1b8b761ce584be6583527d90b39718f3d2b2f
Chris MacDermaid <chris.macdermaid@gmail.com>
2015-01-17 14:27:10 -0500

    Added automatic loading of lammps data files to mollwrapper

--- molwraper.tcl
+++ molwraper.tcl
@@ -12,8 +12,15 @@ proc moll { args } {
 
         new {
             set filename [lindex $newargs 0]
-            catch {mol new [file normalize [glob $filename]]\
-                       {*}[lrange $newargs 1 end]} retval
+
+            if {[string match "*.data" $filename]} {
+                ## Assume lammps data
+                catch {topo readlammpsdata\
+                  [file normalize [glob $filename]]} retval
+            } else {
+                catch {mol new [file normalize [glob $filename]]\
+                           {*}[lrange $newargs 1 end]} retval
+            }
         }
 
         addfile {
@@ -37,35 +44,35 @@ proc moll { args } {
             }
         }
 
-	xst -
+        xst -
         xsc {
 
             ## Load an XSC file, quite a bit more convenient than pbctools
-	    set filename [lindex $newargs 0]
-	    if {[catch {exec tail -1 $filename} data]} {
-		vmdcon -err "$data"
-		return -code error -1
-	    }
+            set filename [lindex $newargs 0]
+            if {[catch {exec tail -1 $filename} data]} {
+                vmdcon -err "$data"
+                return -code error -1
+            }
 
-	    lassign $data id ax ay az bx by bz cx cy cz
-	    if {[catch {molinfo top set {a b c} [list $ax $by $cz]} msg]} {
-		vmdcon -err "Can't set box dimensions: $msg"
-		return -code error -1
-	    }
+            lassign $data id ax ay az bx by bz cx cy cz
+            if {[catch {molinfo top set {a b c} [list $ax $by $cz]} msg]} {
+                vmdcon -err "Can't set box dimensions: $msg"
+                return -code error -1
+            }
 
-	    ## Provide the dimensions as a return value
-	    set retval [list $ax $by $cz]
-	}
+            ## Provide the dimensions as a return value
+            set retval [list $ax $by $cz]
+        }
 
-	reload {
-	    if {[lsearch -ascii [info procs] reload] < 0} {
-		return -code 1 "Reload command currently unavailable"
-	    }
+        reload {
+            if {[lsearch -ascii [info procs] reload] < 0} {
+                return -code 1 "Reload command currently unavailable"
+            }
 
-	    set retval [reload {*}$newargs]
-	}
+            set retval [reload {*}$newargs]
+        }
 
-	default {set retval [mol {*}$args]}
+        default {set retval [mol {*}$args]}
 
     }
 
