87312d489a83d21d9a7bed79e698292d1a295729
Chris MacDermaid <chris.macdermaid@gmail.com>
2015-03-06 10:19:57 -0500

    Updated lipid macro to include all charmm lipid types

--- atomselect_macros.tcl
+++ atomselect_macros.tcl
@@ -21,9 +21,15 @@ atomselect macro sodium { name "Na\+" or resname "NA" }
 atomselect macro sc { resname CLOL CHL1 "CER.*" BENA BENP }
 
 # added Jerome's lipids and cholesterol
-atomselect macro lipid { resname DLPE DMPC GPC LPPC PALM PC PGCL POPC POUC POPE CHL1 CLOL DPPC SAPC }
+#atomselect macro lipid { resname DLPE DMPC GPC LPPC PALM PC PGCL POPC POUC POPE CHL1 CLOL DPPC SAPC }
+
+## All the lipids from CHAMM36 topologies
+atomselect macro lipid { resname LPPC DLPC DLPE DLPS DLPA DLPG DMPC DMPE DMPS DMPA DMPG DPPC DPPE DPPS DPPA
+    DPPG DSPC DSPE DSPS DSPA DSPG DOPC DOPE DOPS DOPA DOPG POPC POPE POPS POPA POPG SAPC SDPC SOPC DAPC CLOL CHL1}
 atomselect macro lnoh { lipid noh }
 
 atomselect macro pchead {lipid and name N C13 C14 C15 C12 C11 P O13 O14 O11 O12 C1 C2 O21 C21 O22 C22 C3 O31 C31 C23 }
+
+
 
 ## NAVAB
